18c8225d49cf8eb61660bcb9abe7ca540a29c4b0
Yevgenii <126769681+uncle-Jonik@users.noreply.github.com>
2024-04-03 21:07:52 +0300

    server init + health check

---
+++ app.js
@@ -0,0 +1,48 @@
+// import express from "express";
+// import morgan from "morgan";
+// import cors from "cors";
+
+// import contactsRouter from "./routes/contactsRouter.js";
+
+// const app = express();
+
+// app.use(morgan("tiny"));
+// app.use(cors());
+// app.use(express.json());
+
+// app.use("/api/contacts", contactsRouter);
+
+// app.use((_, res) => {
+//   res.status(404).json({ message: "Route not found" });
+// });
+
+// app.use((err, req, res, next) => {
+//   const { status = 500, message = "Server error" } = err;
+//   res.status(status).json({ message });
+// });
+
+// app.listen(3000, () => {
+//   console.log("Server is running. Use our API on port: 3000");
+// });
+
+import express from "express";
+import cors from "cors";
+import { nanoid } from "nanoid";
+import { promises as fs } from "fs";
+
+const app = express();
+
+/**
+ * health-check
+ */
+app.get("/ping", (req, res) => {
+  res.status(200).json({ status: "success", data: "Hello API!" });
+});
+
+/**
+ * server-init
+ */
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running. Use our API on port: ${port}`);
+});

34ea1cfd961293706e3afed0a485bfdc884c25d9
Yevgenii <126769681+uncle-Jonik@users.noreply.github.com>
2024-04-20 22:09:06 +0300

    finaly?

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,15 +1,13 @@
-import {
-  logInUserService,
-  logOutUserService,
-} from "../services/authServices.js";
+import { logOutUserService } from "../services/authServices.js";
+import { logInUserService } from "../services/authServices.js";
 import { registerUserService } from "../services/authServices.js";
-import { getContactByIdServices } from "../services/contactsServices.js";
 
 export const registerController = async (req, res, next) => {
   try {
     const newUser = await registerUserService(req.body);
+    const { email, subscription } = newUser;
 
-    res.status(201).json({ user: newUser });
+    res.status(201).json({ user: { email, subscription } });
   } catch (e) {
     next(e);
   }
@@ -18,8 +16,9 @@ export const registerController = async (req, res, next) => {
 export const logInController = async (req, res, next) => {
   try {
     const user = await logInUserService(req.body);
+    const { token, email, subscription } = user;
 
-    res.status(200).json({ user });
+    res.status(200).json({ token, user: { email, subscription } });
   } catch (e) {
     next(e);
   }
@@ -31,6 +30,16 @@ export const logOutController = async (req, res, next) => {
     await logOutUserService(id);
 
     res.sendStatus(204);
+  } catch (e) {
+    next(e);
+  }
+};
+
+export const currentUserController = async (req, res, next) => {
+  try {
+    const { email, subscription } = req.user;
+
+    res.status(200).json({ user: { email, subscription } });
   } catch (e) {
     next(e);
   }
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -6,11 +6,13 @@ import { checkRegisterData } from "../middleware/authMiddleware.js";
 import { logInController } from "../controllers/authControllers.js";
 import { logOutController } from "../controllers/authControllers.js";
 import { registerController } from "../controllers/authControllers.js";
+import { currentUserController } from "../controllers/authControllers.js";
 
 const router = Router();
 
 router.post("/register", checkRegisterData, registerController);
 router.post("/login", checkLogInData, logInController);
+router.get("/current", protect, currentUserController);
 router.post("/logout", protect, logOutController);
 
 export { router };
